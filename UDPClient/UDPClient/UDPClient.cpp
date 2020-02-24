38f9debc9219bd67cde6815e52f0bf1fede8fc4a
kagannikita <kagan.nikita@bk.ru>
2020-02-24 18:54:20 +0600

    first commit

---
+++ UDPClient/UDPClient/UDPClient.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#pragma comment (lib, "ws2_32.lib")
+using namespace std;
+void main(int argc, char* argv[])
+{
+	WSADATA data;
+	setlocale(LC_ALL, "Russian");
+	WORD version = MAKEWORD(2, 2);
+	int wsOk = WSAStartup(version, &data);
+	if (wsOk != 0)
+	{
+		cout << "Winsock doesn't work " << wsOk;
+		return;
+	}
+	sockaddr_in server;
+	server.sin_family = AF_INET; 
+	server.sin_port = htons(54000); 
+	inet_pton(AF_INET, "127.0.0.1", &server.sin_addr); 
+	SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
+	string s(argv[1]);
+	cout << "Enter message: ";
+	getline(cin, s);
+	int sendOk = sendto(out, s.c_str(), s.size() + 1, 0, (sockaddr*)& server, sizeof(server));
+	if (sendOk == SOCKET_ERROR)
+	{
+		cout << "Doesn't Work" << WSAGetLastError() << endl;
+	}
+	else  {
+		cout << "Message received" << endl;
+	}
+	closesocket(out);
+	WSACleanup();
+}
---
+++ UDPServer/UDPServer/UDPServer.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>
+#pragma comment (lib, "ws2_32.lib")
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	struct timeval tv;
+	fd_set fd;
+	WSADATA data;
+	WORD version = MAKEWORD(2, 2);
+	int wsOk = WSAStartup(version, &data);
+	if (wsOk != 0)
+	{
+		cout << "Winsock doesn't work" << wsOk;
+		return;
+	}
+
+	cout << "Server open 5 seconds" << endl;
+	SOCKET in = socket(AF_INET, SOCK_DGRAM, 0);
+	sockaddr_in serverHint;
+	serverHint.sin_addr.S_un.S_addr = ADDR_ANY;
+	serverHint.sin_family = AF_INET;
+	serverHint.sin_port = htons(54000);
+	if (bind(in, (sockaddr*)& serverHint, sizeof(serverHint)) == SOCKET_ERROR)
+	{
+		cout << "Can not bind socket" << WSAGetLastError() << endl;
+		return;
+	}
+	sockaddr_in client;
+	int clientLength = sizeof(client);
+	char buf[1024];
+	while (true)
+	{
+		FD_ZERO(&fd);
+		FD_SET(in, &fd);
+		tv.tv_sec = 5;
+		tv.tv_usec = 0;
+		if (select(0, &fd, NULL, NULL, &tv) > 0)
+		{
+			ZeroMemory(&client, clientLength);
+			ZeroMemory(buf, 1024);
+			int bytesIn = recvfrom(in, buf, 1024, 0, (sockaddr*)& client, &clientLength);
+			if (bytesIn == SOCKET_ERROR)
+			{
+				cout << "Error" << WSAGetLastError() << endl;
+				continue;
+			}
+			char clientIp[256];
+			ZeroMemory(clientIp, 256);
+			inet_ntop(AF_INET, &client.sin_addr, clientIp, 256);
+			cout << "Message received from" << clientIp << " : " << buf << endl;
+		}
+		else {
+			cout << "Timeout" << endl;
+			closesocket(in);
+			WSACleanup();
+			//Sleep(13000);
+			main();
+
+		}
+
+	}
+}
